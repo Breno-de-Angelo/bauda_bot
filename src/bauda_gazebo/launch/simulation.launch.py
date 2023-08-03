74f3f74f4173ddfb622e48342a823986e35c74ef
Breno-de-Angelo <brenodeangelo@gmail.com>
2023-08-03 17:10:59 -0300

    akm xacro

---
+++ src/bauda_gazebo/launch/simulation.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+    package_name='description_bauda_akm'
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+    ])
