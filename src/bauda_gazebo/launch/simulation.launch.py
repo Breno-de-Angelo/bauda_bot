d276ca7bc1f2ea4c32ed29518ee687d38877fe3c
Breno de Angelo <brenodeangelo@gmail.com>
2023-08-11 21:12:25 -0300

    velodyne

--- src/bauda_gazebo/launch/simulation.launch.py
+++ src/bauda_gazebo/launch/simulation.launch.py
@@ -2,34 +2,44 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
-
-
+from launch.event_handlers import OnExecutionComplete
+from launch.actions import RegisterEventHandler
 
 def generate_launch_description():
 
-    package_name='bauda_gazebo'
-
+    package_name = 'bauda_gazebo'
+    package_path = get_package_share_directory(package_name)
+    
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            package_path, 'launch', 'rsp.launch.py'
+        )]),
+        launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    gazebo_world_file = os.path.join(package_path, 'world', 'obstacles.world')
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+            launch_arguments={'pause': 'false', 'world' : gazebo_world_file}.items()
+    )
 
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot', '-z', '0.05'],
+        output='screen')
+    
+    # delay_spawn_entity_after_rsp = RegisterEventHandler(
+    #     OnExecutionComplete(
+    #         target_action=rsp,
+    #         on_completion=[spawn_entity]
+    #     )
+    # )
     
     drive_spawner = Node(
         package="controller_manager",
@@ -43,11 +53,17 @@ def generate_launch_description():
         arguments=["joint_broad"]
     )
 
+    # lidar_joint_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["lidar_joint_controller"]
+    # )    
+
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
         drive_spawner,
-        joint_broad_spawner
+        joint_broad_spawner,
     ])
